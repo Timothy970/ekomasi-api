3b831c54a10774e33be510e1ab7d8800a7540da7
Timothy Kimani <timothy.kimani@emalify.com>
2025-09-08 09:04:27 +0300

    feat: Add parent_id column to menu_links table and comment out foreign key constraints

--- migrations/025_create_table_menu_links.up.sql
+++ migrations/025_create_table_menu_links.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS  `menu_links` (
    `id` int NOT NULL AUTO_INCREMENT,
+   `parent_id` int DEFAULT NULL,
    `title` varchar(100) NOT NULL,
    `url` varchar(255) NOT NULL,
    `display_order` int NOT NULL DEFAULT '0',
