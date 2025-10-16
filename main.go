ea923ea3ae6dbc9fba4143f2e7080cd0f3032f9e
Timothy Kimani <timothy.kimani@emalify.com>
2025-10-16 15:35:00 +0300

    Update CORS middleware to allow all origins

--- main.go
+++ main.go
@@ -192,9 +192,9 @@ func main() {
 	}
 	// CORS middleware
 	corsHandler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:3001"}, // Specify exact origins
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH"},          // Allow specific HTTP methods
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},                  // Allow specific headers
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:3001", "*"}, // Specify exact origins
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH"},               // Allow specific HTTP methods
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},                       // Allow specific headers
 		AllowCredentials: true,
 	}).Handler(router)
 
