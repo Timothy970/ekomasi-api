dd93ddccd81d90af56591da992b109b13b926dfa
Timothy Kimani <timothy.kimani@emalify.com>
2025-12-01 18:04:11 +0300

    Fix variable naming consistency in GetUserLogs: update start_date and end_date to startDate and endDate for improved readability.

--- handlers/logging.go
+++ handlers/logging.go
@@ -22,10 +22,10 @@ func GetUserLogs(w http.ResponseWriter, r *http.Request) {
 	module := r.URL.Query().Get("module")
 	status := r.URL.Query().Get("status")
 	role := r.URL.Query().Get("role")
-	start_date := r.URL.Query().Get("start_date")
-	end_date := r.URL.Query().Get("end_date")
+	startDate := r.URL.Query().Get("start_date")
+	endDate := r.URL.Query().Get("end_date")
 	q := r.URL.Query().Get("q")
-	logs, meta, err := models.GetUserLogs(page, limit, module, status, role, start_date, end_date, q)
+	logs, meta, err := models.GetUserLogs(page, limit, module, status, role, startDate, endDate, q)
 	if err != nil {
 		utils.RespondWithError(w, utils.ErrorJSONResponseOptions{
 			CollectiveInfo: utils.CollectiveInfo{
