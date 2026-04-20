361d575b097246f4058b766e2e1859b7f204de83
Timothy Kimani <timothy.kimani@emalify.com>
2026-04-20 11:25:44 +0300

    fix: Correct SQL syntax for product_variant_combination_options table creation

--- migrations/089_create_product_variants_combinations_options.up.sql
+++ migrations/089_create_product_variants_combinations_options.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS product_variant_combination_options (
     combination_id char(36) NOT NULL,
     variant_id char(36) NOT NULL,
     FOREIGN KEY (combination_id) REFERENCES product_variant_combinations(id) ON DELETE CASCADE,
-    FOREIGN KEY (variant_id) REFERENCES variants(variant_id) ON DELETE CASCADE
-    INDEX idx_combination_id (combination_id),
+    FOREIGN KEY (variant_id) REFERENCES variants(variant_id) ON DELETE CASCADE,
+    INDEX idx_combination_id (combination_id)
 );
